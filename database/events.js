9123bd11c349047c6e407c34f5faa7b1d6d28b44
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-04-24 19:52:07 -0700

    created database using MongoDB and mongoose; seeded database with SF blockchain events

--- database/events.js
+++ database/events.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://database/events');
 
-const eventsSchema = mongoose.Schema({
-  id:
-
+const eventSchema = mongoose.Schema({
+  id: {type: String, unique: true},
+  url: String,
 });
+
+const Event = mongoose.model('Events', eventSchema);
+
+let save = (events) => {
+  console.log('DB length', events.length)
+  let event;
+  for (var i = 0; i < events.length; i++) {
+    event = new Event ({
+      id: events[i].id,
+      url: events[i].url
+    })
+
+    event.save((err, story) => {
+      // if (err) return console.error(err);
+    });
+  }
+}
+
+module.exports.save = save;
 
 
--- worker.js
+++ worker.js
@@ -1,11 +1,17 @@
 // const mongoose = require('mongoose');
 const request = require('request');
+const mongoose = require('mongoose');
 const config = require('./config.js');
+const database = require('./database/events.js');
+mongoose.connect('mongodb://localhost/events');
 
 const getEventsFromEB = () => {
   console.log('KEY', config.TOKEN)
   let events = {
-    url: `https://www.eventbriteapi.com/v3/events/search/?token=${config.TOKEN}?q=${'blockchain'}`,
+    // url: `https://www.eventbriteapi.com/v3/events/search/?q=${'blockchain'}&token=${config.TOKEN}`,
+    url: `https://www.eventbriteapi.com/v3/events/search/?token=${config.TOKEN}
+    &location.address=${'944 Market St. San Francisco, CA, 94102'}
+    &q=${'blockchain'}`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `Bearer ${config.TOKEN}`
@@ -15,20 +21,25 @@ const getEventsFromEB = () => {
   request(events, (err, response, body) => {
     console.log('error:', err); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', JSON.parse(body).events[0].venue_id); // Print the HTML for the Google homepage.
-    let venue = JSON.parse(body).events[0].venue_id
-    let location = {
-      url: `https://www.eventbriteapi.com/v3/venues/${venue}/?token=${config.TOKEN}`,
-      headers: {
-        'User-Agent': 'request',
-        'Authorization': `Bearer ${config.TOKEN}`
-      }
-    }
-    request(location, (err, response, body) => {
-      console.log('error:', err); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', JSON.parse(body).address.city); 
-    })
+    // console.log('body: length ', JSON.parse(body).events); // Print the HTML for the Google homepage.
+    let events = JSON.parse(body).events
+    console.log('LENGTH of', events.length);
+    // console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+    // let venue = JSON.parse(body).events[2].venue_id
+    // let location = {
+    //   url: `https://www.eventbriteapi.com/v3/venues/${venue}/?token=${config.TOKEN}`,
+    //   headers: {
+    //     'User-Agent': 'request',
+    //     'Authorization': `Bearer ${config.TOKEN}`
+    //   }
+    // }
+    // request(location, (err, response, body) => {
+    //   console.log('error:', err); // Print the error if one occurred
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   // console.log('body:', JSON.parse(body).address.city); 
+    //   console.log('body:', JSON.parse(body)); 
+    // })
+    database.save(events);
   });
 }
 
